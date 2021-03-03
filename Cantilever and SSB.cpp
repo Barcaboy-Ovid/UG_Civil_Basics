848ad8a0f3f3af7d45636b939add3ed17033071a
Barcaboy-Ovid <79948496+Barcaboy-Ovid@users.noreply.github.com>
2021-03-03 06:18:58 +0530

    Update Cantilever and SSB.cpp

--- Cantilever and SSB.cpp
+++ Cantilever and SSB.cpp
@@ -1,5 +1,4 @@
 /******************************************************************************
-
                             Online C Compiler.
                 Code, Compile, Run and Debug C program online.
 Write your code in this editor and press "Run" button to compile and execute it.
@@ -201,5 +200,3 @@ else
 }
 
 return 0;
-
-*sss*
