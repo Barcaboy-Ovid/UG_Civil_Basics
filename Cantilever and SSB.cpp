e1dd75264766625d31faa9b2833740a3b9bebae5
Anto Ovid <antoovid98@gmail.com>
2021-03-03 05:37:28 +0530

    Updated name

--- Cantilever and SSB.cpp
+++ Cantilever and SSB.cpp
@@ -3,7 +3,7 @@
                             Online C Compiler.
                 Code, Compile, Run and Debug C program online.
 Write your code in this editor and press "Run" button to compile and execute it.
-
+Author: Anto Ovid
 *******************************************************************************/
 
 #include<stdio.h>
@@ -202,4 +202,4 @@ else
 
 return 0;
 
-}
+*sss*
