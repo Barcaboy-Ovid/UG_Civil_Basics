bd10de47f08ee57ac7031b45b9a38c3931a4c443
Barcaboy-Ovid <79948496+Barcaboy-Ovid@users.noreply.github.com>
2021-03-05 23:53:41 +0530

    Update Cantilever and SSB.cpp
    Edited

--- Cantilever and SSB.cpp
+++ Cantilever and SSB.cpp
@@ -1,7 +1,6 @@
 /******************************************************************************
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
+                This code is written to calculate Cantilever and Simply supported beams
+                          Maximum Bending moment, Deflection and Slope 
 Author: Anto Ovid
 *******************************************************************************/
 
